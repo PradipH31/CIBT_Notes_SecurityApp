acee79afc02a6836b387beb8e42785de7692551b
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-07 17:25:43 +0545

    No Security configuration

--- src/main/java/com/cibt/web/configuration/SecurityConfiguration.java
+++ src/main/java/com/cibt/web/configuration/SecurityConfiguration.java
@@ -6,6 +6,7 @@
 package com.cibt.web.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -16,5 +17,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    }
     
 }
---
+++ src/main/java/com/cibt/web/controller/DahsboardController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cibt.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author HP B&O
+ */
+@Controller
+@RequestMapping(value = "/admin")
+public class DahsboardController {
+    @GetMapping
+    @ResponseBody
+    public String index(){
+        return "<h1>Secured</h1>";
+    }
+}
