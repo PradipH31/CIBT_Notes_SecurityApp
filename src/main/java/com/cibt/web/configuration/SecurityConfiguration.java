585569b3098c9c5376c9e4549bb7c3969eae4067
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-07 18:50:56 +0545

    Added login credentials in memory

--- src/main/java/com/cibt/web/configuration/SecurityConfiguration.java
+++ src/main/java/com/cibt/web/configuration/SecurityConfiguration.java
@@ -5,11 +5,14 @@
  */
 package com.cibt.web.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  *
@@ -31,4 +34,17 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/webjars/**");
     }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(bCryptPasswordEncoder())
+                .withUser("admin")
+                .password(bCryptPasswordEncoder().encode("admin1234"))
+                .roles("USER");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
