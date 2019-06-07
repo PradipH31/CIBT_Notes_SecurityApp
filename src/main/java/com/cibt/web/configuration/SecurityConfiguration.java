51f085d0eaf78e879f22895ffd4b60029e5c912d
Pradip Hamal <pradip.hamal31@gmail.com>
2019-06-07 17:32:07 +0545

    Set home to be accessed by all & other pages to redirect to springLogin form

--- src/main/java/com/cibt/web/configuration/SecurityConfiguration.java
+++ src/main/java/com/cibt/web/configuration/SecurityConfiguration.java
@@ -20,6 +20,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().permitAll();
     }
-    
+
 }
